98cbf6c5667eb65424345208137d07ed764368b9
c j <jichao99@outlook.com>
2021-03-09 19:01:20 +0800

    mobile init

---
+++ CheckKeywordsMobile/CheckKeywordsMobile/App.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.MobileBlazorBindings;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace CheckKeywordsMobile
+{
+    public class App : Application
+    {
+        public App(IFileProvider fileProvider = null)
+        {
+            var hostBuilder = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Adds web-specific services such as NavigationManager
+                    services.AddBlazorHybrid();
+                    services.AddScoped(sp => new HttpClient { });
+                    services.AddAntDesign();
+                    // Register app-specific services
+                    services.AddSingleton<CounterState>();
+                })
+                .UseWebRoot("wwwroot");
+
+            if (fileProvider != null)
+            {
+                hostBuilder.UseStaticFiles(fileProvider);
+            }
+            else
+            {
+                hostBuilder.UseStaticFiles();
+            }
+            var host = hostBuilder.Build();
+
+            MainPage = new ContentPage { Title = "CheckKeywords" };
+            MainPage.BackgroundColor = Color.Orange;
+            
+            host.AddComponent<Main>(parent: MainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
